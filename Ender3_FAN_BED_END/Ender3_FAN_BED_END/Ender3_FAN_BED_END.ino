9c107c5cd54b052ebcd08edcba62720aee0d6a89
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-08-21 20:19:36 +0900

    FAN and BED and END(Nozzle)
    FAN 과 BED와 END(Nozzle)을 제어

---
+++ Ender3_FAN_BED_END/Ender3_FAN_BED_END/Ender3_FAN_BED_END.ino
@@ -0,0 +1,48 @@
+﻿#define FAN	0x10	//PB4
+
+#define BED_TEMP A6		//PA6 (Analog_Pin 6)
+#define HOT_BED	0x10	//PD4
+
+#define END_TEMP A7		//PA7 (Analog_Pin 7)
+#define HOT_END	0x20	//PD5
+
+
+void setup() {
+	//FAN
+	DDRB |= FAN;
+	PORTB |= FAN;
+
+	//BED
+	DDRD |= BED_TEMP;
+	//PORTD |= BEDTEMP;
+
+	//END (Nozzle)
+	DDRD |= END_TEMP;
+	PORTD |= END_TEMP;
+	DDRD |= HOT_END;
+	PORTD |= HOT_END;
+}
+
+
+void loop() {
+	//FAN
+	PORTB |= FAN;
+	delay(2000);
+	PORTB & ~FAN;
+	delay(2000);
+
+	//BED
+	int bed_analog_value = analogRead(BED_TEMP);
+	Serial.println(bed_analog_value);
+
+	if (bed_analog_value < 920) PORTD &= ~HOT_BED;
+	delay(300);
+
+	//END (Nozzle)
+	int end_analog_value = analogRead(END_TEMP);
+	Serial.println(end_analog_value);
+
+	if (end_analog_value <= 100) PORTD &= ~HOT_END;
+	else if (end_analog_value > 105) PORTD |= HOT_END;
+	delay(300);
+}
