f4eb72979c044765f28ce3d658229bef63d56249
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-08-27 18:56:58 +0900

    Extruder

---
+++ Ender3_Extruder/Ender3_Extruder/Ender3_Extruder.ino
@@ -0,0 +1,35 @@
+﻿#define XYEENABLE 0x40  // PD6
+
+#define E_STEP 0x02  //PB1
+#define E_DIR 0x01	 //PB0
+
+void setup() {
+	//Extruder (E)
+	DDRB |= E_STEP;
+
+	//Extruder TIMER
+	TCCR2A = 0x02;
+	TCCR2B = 0x05;
+	TCNT2 = 0x00;
+	//OCR2A = 256 - 167; // 334 us per interrupt ( CNT - 167 )
+	OCR2A = 245;
+	TIMSK2 = 0x02;	//0x02 : TIMER ON
+}
+
+
+void loop() {
+	
+}
+
+volatile char e_step_toggle = 0;
+
+ISR(TIMER2_COMPA_vect) {
+	if (e_step_toggle == 0) {
+		e_step_toggle = 1;
+		PORTB |= E_STEP;
+	}
+	else {
+		e_step_toggle = 0;
+		PORTB &= ~E_STEP;
+	}
+}
